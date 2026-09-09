830e9bddf4643755f2e6b2966b53c5206ecb030c
jasonleetucker-code <jasonleetucker@icloud.com>
2026-09-09 09:49:11 -0400

    test(steward): prove context fingerprint determinism

---
+++ tests/steward/test_context.py
@@ -0,0 +1,7 @@
+from src.steward.context import ContextDocument, bundle
+
+def test_context_bundle_is_order_independent_and_preserves_provenance():
+    a = ContextDocument("a", "v1", "one")
+    b = ContextDocument("b", "v2", "two")
+    assert bundle([a, b]) == bundle([b, a])
+    assert bundle([a, b])["documents"] == [{"identifier": "a", "version": "v1"}, {"identifier": "b", "version": "v2"}]
